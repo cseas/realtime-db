28ad993ccd00b0f39e6f844a174c16aa42a0ed7e
abhijeetstomar <contact.abhijeetsingh@gmail.com>
2018-06-25 02:29:20 +0530

    Removed redundant code

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 (function() {
   // Initialize Firebase
   const config = {
-    apiKey: "AIzaSyCU4teCV7bcq7Ok9WrPfEY9QU5KyGQIuVI",
-    authDomain: "hello-5d780.firebaseapp.com",
-    databaseURL: "https://hello-5d780.firebaseio.com",
-    projectId: "hello-5d780",
-    storageBucket: "hello-5d780.appspot.com",
-    messagingSenderId: "703637547804"
+    apiKey: "",
+    authDomain: "<yourapp>.firebaseapp.com",
+    databaseURL: "https://<yourapp>.firebaseio.com",
+    projectId: "<yourapp>",
+    storageBucket: "<yourapp>.appspot.com",
+    messagingSenderId: ""
   };
   firebase.initializeApp(config);
 
